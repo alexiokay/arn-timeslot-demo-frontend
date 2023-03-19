99a227b972d1db9c9303e644974802fd3fd82435
papadi166 <papadi166@gmail.com>
2023-03-19 20:33:00 +0100

    arrow's settings updated

--- components/Settings/Arrow.vue
+++ components/Settings/Arrow.vue
@@ -1,12 +1,50 @@
 <template lang="pug">
 SettingsPanel(:title="props.title" @save="save()")
     template(v-slot:content)
-        div(class="flex w-4/5 space-x-[4rem] items-center ")
-            div(class="flex flex-col w-2/3")
+        div(class="flex w-4/5 space-x-[2rem] items-start ")
+            div(class="flex flex-col w-1/3")
                 p(class="font-semibold text-gray-700") Workable times
            
                 p(class="text-gray-600 text-sm") Here you can set your default workable times. These times will be used for all your future bookings.
-            div(class="flex flex-col w-full  space-y-4  justify-center items-start ")
+            div(class="flex flex-col w-auto  space-y-4  justify-center items-start ")
+                div(class="flex flex-col w-auto  justify-center items-start ")
+                    p Weekdays
+                    hr(class="w-full border-gray-200 border-[1px]")
+                div(class="flex w-auto space-x-2")
+                    label(class="flex items-center space-x-2")  open:
+                    input(type="checkbox" v-model="settingsStore.workable_hours.is_open" class="")
+                div(class="flex  w-full items-center space-x-2")
+                    label(data-te-select-label-ref class="w-[3rem]") from:
+                    select(data-te-select-init class="h-[2rem]  w-[4rem]" v-model="settingsStore.workable_hours.from")
+                        option(v-for="i in 24" :key="i") {{ i }}:00
+                        
+                div(class="flex  w-full items-center justify-start space-x-2")
+                    label(data-te-select-label-ref class="w-[3rem]") to:
+                    select(data-te-select-init class="h-[2rem]  w-[4rem]" v-model="settingsStore.workable_hours.to")
+                        option(v-for="i in 24" :key="i") {{ i }}:00
+            div(class="flex flex-col w-auto  space-y-4  justify-center items-start ")
+                div(class="flex flex-col w-auto  justify-center items-start ")
+                    p Saturdays
+                    hr(class="w-full border-gray-200 border-[1px]")
+                div(class="flex w-auto space-x-2")
+                    label(class="flex items-center space-x-2") open:
+                    input(type="checkbox" v-model="settingsStore.workable_hours.is_saturday_open" class="")
+                div(class="flex  w-full items-center space-x-2")
+                    label(data-te-select-label-ref class="w-[3rem]") from:
+                    select(data-te-select-init class="h-[2rem]  w-[4rem]" v-model="settingsStore.workable_hours.from")
+                        option(v-for="i in 24" :key="i") {{ i }}:00
+                        
+                div(class="flex  w-full items-center justify-start space-x-2")
+                    label(data-te-select-label-ref class="w-[3rem]") to:
+                    select(data-te-select-init class="h-[2rem]  w-[4rem]" v-model="settingsStore.workable_hours.to")
+                        option(v-for="i in 24" :key="i") {{ i }}:00
+            div(class="flex flex-col w-auto  space-y-4  justify-center items-start ")
+                div(class="flex flex-col w-auto  justify-center items-start ")
+                    p Sundays
+                    hr(class="w-full border-gray-200 border-[1px]")
+                div(class="flex w-auto space-x-2")
+                    label(class="flex items-center space-x-2") open:
+                    input(type="checkbox" v-model="settingsStore.workable_hours.is_sunday_open" class="")
                 div(class="flex  w-full items-center space-x-2")
                     label(data-te-select-label-ref class="w-[3rem]") from:
                     select(data-te-select-init class="h-[2rem]  w-[4rem]" v-model="settingsStore.workable_hours.from")
@@ -19,11 +57,11 @@ SettingsPanel(:title="props.title" @save="save()")
         hr(class="w-full border-gray-200 border-[1px]")
         
         div(class="flex w-4/5 space-x-[4rem] items-center ")
-            div(class="flex flex-col w-2/3")
+            div(class="flex flex-col w-1/3")
                 p(class="font-semibold text-gray-700") Max amount of trucks
            
                 p(class="text-gray-600 text-sm") Here you can set your default max amount of trucks. This amount will be used for all your future bookings.
-            div(class="flex flex-col w-full space-x-8 space-y-4 ")
+            div(class="flex flex-col w-auto space-x-8 space-y-4 ")
                 input(class="h-[2rem] w-[3.6rem]  pl-2" type="number"  v-model="settingsStore.max_trucks_per_hour" min="1" max="10")
                     
         
